74ce9ba2ea10f47c94b2614c006ba316ebde5ba6
Jakob Neufeld <jakob.n.neufeld@gmail.com>
2019-12-10 20:18:19 +0100

    Initial Commit

---
+++ Winery/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Winery
+//
+//  Created by Niko Neufeld on 10.12.19.
+//  Copyright © 2019 Niko Neufeld. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
